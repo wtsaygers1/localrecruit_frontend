248233a60af51f4118d68423e83df22b6096af9d
wtsaygers1 <wtsaygers1@gmail.com>
2021-04-29 11:07:52 -0400

    navbar NavLinks connected

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 export default function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">LocalRecruit</a>
+                <NavLink className="navbar-brand" to="/">LocalRecruit</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" href="#">About</Link>
+                            <NavLink className="nav-link active" aria-current="page" to="/about" activeClassName="active">About</NavLink>
                         </li>
-                        {/* <li className="nav-item">
-                            <Link className="nav-link" href="#"></Link>
-                        </li> */}
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Sports</a>
@@ -29,8 +26,8 @@ export default function Header() {
                         </li>
                     </ul>
                     <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Not Functional" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
+                        <button className="btn btn-outline-success" type="submit"><NavLink to="/register">Register</NavLink></button>
+                        <button className="btn btn-outline-success" type="submit"><NavLink to="/login">Login</NavLink></button>
                     </form>
                 </div>
             </div>
