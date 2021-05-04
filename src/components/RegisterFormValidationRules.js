cd699f5161debf7f37b88bc1a654579bc29fb7f9
wtsaygers1 <wtsaygers1@gmail.com>
2021-05-04 10:32:45 -0400

    color on validation

--- src/components/RegisterFormValidationRules.js
+++ src/components/RegisterFormValidationRules.js
@@ -7,24 +7,19 @@ export default function validate(values) {
     } else if(!/\S+@\S+\.\S/.test(values.email)) {
         errors.email = 'Please enter valid email address';
     }
-    console.log(errors.email);
     if(!values.password){
         errors.password = 'Password is required'
     } else if(values.password.length < 8){
         errors.password = 'Password must be 8 or more characters';
     }
-    console.log(errors.password)
     if(!values.first_name){
         errors.first_name = 'First name is required';
     }
-    console.log(errors.first_name)
     if(!values.last_name){
         errors.last_name = 'Last name is required';
     }
-    console.log(errors.last_name)
     if(!values.date_of_birth){
         errors.date_of_birth = 'Valid date required';
     }
-    console.log(errors.date_of_birth)
     return errors;
 };
