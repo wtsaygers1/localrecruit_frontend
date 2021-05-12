4c61466e2148c32a74f72b7ce2149d85dafdad91
wtsaygers1 <wtsaygers1@gmail.com>
2021-05-11 21:16:11 -0400

    logo being added

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ export default function Header(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light subtext">
             <div className="container-fluid">
-                <NavLink className="navbar-brand" to="/">Sports Ball</NavLink>
+                <NavLink className="navbar-brand" to="/">Sports Ball<img src="beach_ball.jpeg" /></NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
